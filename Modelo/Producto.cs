b67bf45acbffaa4597ca80f497225b0005138b05
BackEnd[Ejrc] <matvidelac@gmail.com>
2018-11-14 23:34:15 -0300

    primer commit

---
+++ Modelo/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string codproducto { get; set; }
+        public string nombreproducto { get; set; }
+        public string tipoproducto { get; set; }
+        public string saldominimo { get; set; }
+        public int estado { get; set; }
+    }
+}
